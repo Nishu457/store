53ad0caaeaac3680a4f98293a3057df904a5b121
Nishu457 <nishunishanth883@gmail.com>
2025-12-17 21:05:16 +0530

    Add Cloudinary cloud storage

--- project1/server.js
+++ project1/server.js
@@ -19,7 +19,7 @@ cloudinary.config({
 });
 
 // Middleware
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set('view engine', 'ejs');
